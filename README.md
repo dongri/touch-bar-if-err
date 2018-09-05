e5fdb42a1357bbc8a3b7a64e3e44a0e9a0f1fa8e
Dongri Jin <dongrify@gmail.com>
2018-09-05 22:56:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Touch Bar if err
+
+easy and fast insert 
+```
+if err != nil {
+
+}
+```
+
+## Installation
+
+```
+$ code --install-extension dongri.touch-bar-if-err
+```
+
+## Release Notes
+
+### 0.0.1
+
+Initial release.
+
+-----------------------------------------------------------------------------------------------------------
+
+# Development
+
+```
+$ npm install -g yo generator-code
+$ yo code
+```
---
+++ extension.js
@@ -0,0 +1,19 @@
+const vscode = require('vscode');
+function activate(context) {
+
+    let disposable = vscode.commands.registerCommand('extension.ifErr', function () {
+        
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+        const position = editor.selection.active;
+        editor.insertSnippet(new vscode.SnippetString('if err != nil {\n\n}'), position);
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+function deactivate() {
+}
+exports.deactivate = deactivate;
